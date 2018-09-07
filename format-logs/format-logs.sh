c1fd53e3cf762d719a3e8e3e26cf5b2b8ca72f62
ramdhakne <ram.dhakne@couchbase.com>
2018-09-07 10:39:47 -0700

    Checkin latest format-logs.sh

--- format-logs/format-logs.sh
+++ format-logs/format-logs.sh
@@ -18,7 +18,8 @@ else
 fi
 
 
-metrics="max_data_size|high_wat|low_wat|mem_used|ep_dbname|ep_value_size|curr_items_tot|ep_num_non_resident|version|vb_active_curr_items|active_perc_mem|replica_perc_mem"
+metrics="ep_max_size|high_wat|low_wat|mem_used|ep_couch_bucket|ep_value_size|curr_items_tot|ep_num_non_resident|version|vb_active_curr_items|active_perc_mem|replica_perc_mem|cmd_get|cmd_set|get_hits|get_misses|vb_active_ops_create|vb_active_ops_update|vb_replica_ops_create|vb_replica_ops_update|ep_bg_fetched|ep_bg_meta_fetched|vb_active_meta_data_memory|vb_replica_meta_data_memory"
+
 tmpStats="stats-`date +%s`"
 tmpResults="results-`date +%s`"
 files="`ls ${path}/*.zip`"
@@ -28,20 +29,22 @@ for file in ${files}
 do
     echo "Working on file: ${file}"
     host=`basename ${file}|cut -d '.' -f1`
+    echo "host: $host"
     statsFile="`unzip -l ${file} |grep stats.log|grep -v ns_server|awk '{print $4;}'`"
-    #clean windows files with col -b
-    unzip -p ${file} ${statsFile} | col -b > ${tmpStats}
+    echo "statsFile: $statsFile"
+    unzip -p ${file} ${statsFile} > ${tmpStats}
     if [ $? -ne 0 ]
     then
         echo "Failed to unpack ${file}"
     fi
+	length=`grep -n 'memcached stats checkpoint' ${tmpStats}|cut -d ':' -f1`
 
-    head -10000 ${tmpStats} |egrep  -i "${metrics}" |grep -v ep_version | while IFS=":" read -r k v
+    head -${length} ${tmpStats} |egrep  -i "${metrics}" |egrep -v 'memcached_version|ep_version' | while IFS=":" read -r k v
     do
         case "$k" in
             *"curr_items_tot"* ) curr_items_tot=`echo $v|sed 's/^ *//g'` ;;
-            *"ep_dbname"* ) ep_dbname=`echo $v| tr '/' ' ' | tr '\\' ' ' | awk '{ print $NF }'` ;;
-            *"ep_max_data_size"* ) ep_max_data_size=`echo $v|sed 's/^ *//g'` ;;
+            *"ep_couch_bucket"* ) ep_couch_bucket=`echo $v|sed 's/^ *//g'` ;;
+            *"ep_max_size"* ) ep_max_size=`echo $v|sed 's/^ *//g'` ;;
             *"ep_mem_high_wat"* ) ep_mem_high_wat=`echo $v|sed 's/^ *//g'` ;;
             *"ep_mem_low_wat"* ) ep_mem_low_wat=`echo $v|sed 's/^ *//g'` ;;
             *"ep_num_non_resident"* ) ep_num_non_resident=`echo $v|sed 's/^ *//g'` ;;
@@ -50,11 +53,25 @@ do
             *"vb_active_perc_mem"* ) vb_active_perc_mem=`echo $v|sed 's/^ *//g'` ;;
             *"vb_replica_perc_mem"* ) vb_replica_perc_mem=`echo $v|sed 's/^ *//g'` ;;
             *"ep_value_size"* ) ep_value_size=`echo $v|sed 's/^ *//g'` ;;
-		    *"version"* ) 
+# cmd_get|cmd_set|get_hits|get_misses|vb_active_ops_create|vb_active_ops_update|vb_replica_ops_create|vb_replica_ops_update
+            *"cmd_get"* ) cmd_get=`echo $v|sed 's/^ *//g'` ;;
+            *"cmd_set"* ) cmd_set=`echo $v|sed 's/^ *//g'` ;;
+            *"get_hits"* ) get_hits=`echo $v|sed 's/^ *//g'` ;;
+            *"get_misses"* ) get_misses=`echo $v|sed 's/^ *//g'` ;;
+            *"vb_active_ops_create"* ) vb_active_ops_create=`echo $v|sed 's/^ *//g'` ;;
+            *"vb_active_ops_update"* ) vb_active_ops_update=`echo $v|sed 's/^ *//g'` ;;
+            *"vb_replica_ops_create"* ) vb_replica_ops_create=`echo $v|sed 's/^ *//g'` ;;
+            *"vb_replica_ops_update"* ) vb_replica_ops_update=`echo $v|sed 's/^ *//g'` ;;
+            *"ep_bg_fetched"* ) ep_bg_fetched=`echo $v|sed 's/^ *//g'` ;;
+            *"ep_bg_meta_fetched"* ) ep_bg_meta_fetched=`echo $v|sed 's/^ *//g'` ;;
+            *"vb_active_meta_data_memory"* ) vb_active_meta_data_memory=`echo $v|sed 's/^ *//g'` ;;
+            *"vb_replica_meta_data_memory"* ) vb_replica_meta_data_memory=`echo $v|sed 's/^ *//g'` ;;
+
+            *"version"* ) 
                  echo \
-                 "${ep_dbname},\
+                 "${ep_couch_bucket},\
                     ${host},\
-                    ${ep_max_data_size},\
+                    ${ep_max_size},\
                     ${ep_mem_high_wat},\
                     ${ep_mem_low_wat},\
                     ${mem_used},\
@@ -63,24 +80,51 @@ do
                     ${curr_items_tot}, \
                     ${ep_num_non_resident},\
                     ${vb_active_perc_mem},\
-                    ${vb_replica_perc_mem}" \
+                    ${vb_replica_perc_mem}, \
+                    Utilization, \
+                    ${cmd_get}, \
+                    ${cmd_set}, \
+                    ${get_hits}, \
+                    ${get_misses}, \
+                    ${vb_active_ops_create}, \
+                    ${vb_active_ops_update}, \
+                    ${vb_replica_ops_create}, \
+                    ${vb_replica_ops_update}, \
+                    ${ep_bg_fetched}, \
+                    ${ep_bg_meta_fetched}, \
+                    ${vb_active_meta_data_memory}, \
+                    ${vb_replica_meta_data_memory}" \
                 >> ${tmpResults}
-			;;
-	    esac
+                echo "" >> ${tmpResults}
+            ;;
+        esac
     done 
     > ${tmpStats}
 done 
 
-echo ",max_data_size,high_wat,low_wat,mem_used,item count,avg value size,ep_value_size,curr_items_tot,ep_num_non_resident,vb_active_perc_mem,vb_replica_perc_mem" >${outputFile}
+echo ",max_data_size,high_wat,low_wat,mem_used,item count,avg value size,ep_value_size,curr_items_tot,ep_num_non_resident,vb_active_perc_mem,vb_replica_perc_mem,Memory Utilization, CMD Get, CMD Set, Get Hits, Get Misses, VB Active Create,VB Active Update, VB Replica Create, VB Replica Update, Disk reads, Meta Disk reads, Active Meta data memory, Replica Meta data memory, total meta data memory, percentage metadata utilization" >${outputFile}
 bucketList="`cat ${tmpResults}|awk '{print $1;}' |sort|uniq`"
+#new
+cat ${tmpResults} |sort|uniq|sed s/' '//g >>${tmpResults}.1
+
 for bucket in ${bucketList}
 do
     echo "${bucket}" >> ${outputFile}
-    grep ${bucket} ${tmpResults} | while read line
+    grep ${bucket} ${tmpResults}.1 | while read line
     do
-        echo ${line} | sed s/${bucket}//g >> ${outputFile}
+        #echo ${line} | sed s/${bucket}//g >> ${outputFile}
+        echo ${line} | grep -q "^,"
+        if [ $? -ne 0 ]
+        then 
+            echo ${line} | sed s/${bucket}//g >> ${outputFile}
+        fi
     done
 done
 echo "Average value size = ep_value_size/(curr_items_tot-ep_num_non_resident)" >> ${outputFile}
+echo "Percentage meta data utilization = (vb_active_meta_data_memory + vb_replica_meta_data_memory) / ep_max_size"
+echo "Utilization = mem_used/ ep_max_size" >> ${outputFile}
 echo "Sizing data saved to: ${outputFile}"
-rm ${tmpStats} ${tmpResults}
+
+# Cleanup
+rm ${tmpResults} ${tmpResults}.1 ${tmpStats}
+
